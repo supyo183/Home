8c5a84ac0470b994191dfd1c2bef8934f2d21024
supyo183 <91503994+supyo183@users.noreply.github.com>
2022-09-29 11:12:56 -0500

    replaced old with new

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function closeNav() {
 }
 
 var i = 0;
-var txt = 'School Project';
+var txt = 'Home';
 var speed = 100;
 
 function typeWriter() {
@@ -36,21 +36,8 @@ function typeWriter() {
   }
 }
 
-/* should not be activated until use is found */
-var countDownDate = new Date("sep 20 2022 11:28:30").getTime();
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
+function e() {
+  alert('Baka! You are already home!')
+}
 
-console.log('sitev0.0.1')
+console.log('sitev0.2.1')
