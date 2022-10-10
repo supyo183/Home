dd9c471a28e32f4af303b8a1fdef83824a1c8d25
supyo183 <91503994+supyo183@users.noreply.github.com>
2022-10-10 09:42:22 -0500

    new look for school-project
    made a new look and disabled the home button for school-project (will reactivate soon)

--- school-project.js
+++ school-project.js
@@ -1,14 +1,3 @@
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -36,21 +25,4 @@ function typeWriter() {
   }
 }
 
-/* should not be activated until use is found */
-var countDownDate = new Date("oct 11 2022 10:33:00").getTime();
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-console.log('sitev1.0.5')
+console.log('sitev1.1.4')
